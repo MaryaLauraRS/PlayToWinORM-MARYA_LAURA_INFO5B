a64aef5d7cd989d4b9a749a50db9987514af40ba
MaryaLauraRS <marya.santos@estudante.ifms.edu.br>
2024-04-19 13:27:45 -0400

    index

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 require("dotenv").config();
 const conn = require("./db/conn");
+
 const Usuario = require("./models/Usuario");
 const Jogo = require("./models/Jogo"); 
+
 const express = require("express");
+
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(`
-    <div>
-      <a href="/usuarios/novo">Cadastrar Usuário</a> <br>
-      <a href="/jogos/novo">Cadastrar Jogo</a>
-    </div>
-  `);
-});
+app.use(
+  express.urlencoded({
+  extended: true,
+  })
+)
+app.use(express.json());
 
 app.get("/usuarios/novo", (req, res) => {
   res.sendFile(`${__dirname}/views/formUsuario.html`);
 });
-
-app.get("/jogos/novo", (req, res) => {
-  res.sendFile(`${__dirname}/views/formJogo.html`);
-});
-
-
 
 app.post("/usuarios/novo", async (req, res) => {
   const nickname = req.body.nickname;
@@ -35,44 +28,38 @@ app.post("/usuarios/novo", async (req, res) => {
     nickname,
     nome,
   };
-
-  try {
     const usuario = await Usuario.create(dadosUsuario);
-    res.send(`<script>alert("Usuário inserido sob o id ${usuario.id}"); window.location.href = '/usuarios/novo';</script>`);
-  } catch (error) {
-    console.error("Erro ao criar usuário:", error);
-    res.send(`<script>alert("Erro ao criar usuário: ${error.message}"); window.location.href = '/usuarios/novo';</script>`);
-  }
+    res.send("Usuário inserido sob o id " + ${usuario.id});
+})
+
+app.get("/jogos/novo", (req, res) => {
+  res.sendFile(`${__dirname}/views/formJogo.html`);
 });
-
-
-
 
 app.post("/jogos/novo", async (req, res) => {
-  const { titulo, descricao, precoBase } = req.body;
+  const titulo = req.body.titulo;
+const descricao = reg-body-descricao;
+const precobase = req.body- precobase;
 
-  try {
-    const jogo = await Jogo.create({
-      titulo,
-      descricao,
-      precoBase,
-    });
-    res.send(`<script>alert("Jogo inserido sob o id ${jogo.id}"); window.location.href = '/jogos/novo';</script>`);
-  } catch (error) {
-    console.error("Erro ao criar jogo:", error);
-    res.send(`<script>alert("Erro ao criar jogo: ${error.message}"); window.location.href = '/jogos/novo';</script>`);
-  }
-});
+const dadosJogo = {
+titulo, 
+descricao, 
+precobase,
+};
 
+const jogo = await Jogo.create (dadosJogo);
+    res.send("Jogo inserido sob o id " + jogo.id);
+})
 
 app.listen(8000, () => {
   console.log("Rodando na porta 8000");
 });
 
-conn.sync()
+conn
+  .sync()
   .then(() => {
     console.log("Banco de dados conectado e estrutura sincronizada!");
   })
   .catch((err) => {
     console.error("Erro ao conectar ao banco de dados:", err);
-  });
+  })
